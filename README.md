deea5ecc187f9882735c3781990f3f4363c3d561
NicksComingUp <niclaus.a@gmail.com>
2018-06-06 16:22:11 -0500

    using a Worker file to consistently check cite availability of check information

--- README.md
+++ README.md
@@ -1,9 +1,69 @@
 Creating a Restful Api
 
-This api while be able to send sms messages to user.
-Create a new User.
-Give the User a token to validate them when so
-that they can use get, put, delete methods for there account.
-Store information using a file system.
-I will not be using external npm libraries for this app.
-This objective of this is to make me appriciate the use of external library such as mongoose and user authen which could be use to speed up development much better.
+Concise of creating a new user with the provided fields
+{
+  'firstName'    : firstName,
+  'lastName'     : lastName,
+  'phone'        : phone,
+  'password'     : hashPassword,
+  'tosAgreement' : true
+}
+The passWord is encripted information by a sha256 code which the string of config.hashPassword.
+
+Creating token verify if the user is log into his account
+to create a token you would use the post method with the following information of an
+existing user record
+{
+  phone : phone,
+  password: password
+}
+The get method gets the information from the user
+the put method updates the user information.
+the delete method remove the user from file with all of there checks information.
+
+
+the password would be encrypted and compare to the password of the user with the phone information.
+then a token will be created with the following information:
+tokenObject = {
+ 'phone'  : phone,
+ 'id'     : tokenId,
+ 'expires': expires
+}
+the expires token is use to see if the token is still running in this program we gave it a 1 hour period
+until the following token expire.
+The id is use as a header in which the user is suppose to know which would verify whether he/she
+is authenticated or not.
+The only method which are available when you do not have a token is the user post and the token post method.
+you can only post a token if you sign in as a user.
+The get method gets method of the token gets token information.
+the put method increase the expiration time of the token by 1 hour.
+the delete method removes the token from the file.
+
+When you have a User login( with the token)
+You are able to create a checks with the post method which contain the following information:
+const checkObject = {
+  'id'            : checkId,
+  'userPhone'     : userPhone,
+  'protocol'      : protocol,
+  'url'           : url,
+  'method'        : method,
+  'successCodes'  : successCodes,
+  'timeoutSecond' : timeoutSecond
+}
+Each check that is created is embedded in the user data with the phone number associated with it.
+the maximum number of check that can be embedded is 5.
+The get method return one check
+the put method update the following properties of the check
+{
+  protocol: protocol,
+  url: url,
+  method =method,
+  successCodes: successCodes,
+  timeoutSecond: timeoutSecond
+}
+the delete method remove one check from the user and from the check file
+The workers is use to check whether the information has change its status to send the user information about the
+change of status by sms messages. 
+
+all information which we have are store in file system and is collected in the .data directory.
+no external npm libraries are use for this api.
--- index.js
+++ index.js
@@ -1,98 +1,24 @@
-const 	http 					= require('http'),
-				https 				= require('https'),
-			  url  					= require('url'),
-				config				= require('./lib/config'),
-				fs						= require('fs'),
-				handlers			= require('./lib/handlers'),
-				helpers 			= require('./lib/helpers'),
-			  StringDecoder = require('string_decoder').StringDecoder;
-
-//
-helpers.sendTwilioSms('', 'Hello', function (err) {
-	console.log('this was the err', err);
-});
-
-//Create Server
-const httpServer = http.createServer((req, res) =>{
- unifidedServer(req,res);
-});
-// Open Port For server to listen
-httpServer.listen(config.httpPort, ()=> {
-		console.log('server is Working '+ config.httpPort );
-});
- // pass these server options to the https server when it start to create a secure server
-const httpsServerOptions = { // files from Openssl
-	'key'  : fs.readFileSync('./https/key.pem'),
-	'cert' : fs.readFileSync('./https/certificate.pem')
-};
-// Create https Server
-const httpsServer = https.createServer(httpsServerOptions, (req, res) =>{
- unifidedServer(req,res);
-});
-
-// Open port for server to Listen
-httpsServer.listen(config.httpsPort, ()=> {
-		console.log('server is Working '+ config.httpsPort );
-});
+/**
+*Private file for the Api
+*
+*/
 
 
-const unifidedServer = function(req, res){
-	// get methods                              // gets an object
-	let method = req.method.toLowerCase(), headers = req.headers;
-	// gets the url
-	let parsedUrl = url.parse(req.url, true);
-	//get the path name             // get the object after the pathname
-	let path = parsedUrl.pathname,  queryStringObject = parsedUrl.query;
-	// take out the extra slashes
-	let trimmedPath = path.replace(/^\/+|\/+$/g,'');
-	// get the route function at the bottom (router [ pathname ]) and store it inside chosenHandler
- 	let chosenHandler = typeof(router[trimmedPath]) !== 'undefined'? router[trimmedPath] : handlers.notFound;
-// this is incredible
-	let decoder = new StringDecoder('utf-8'); // use to convert buffer to string
-	let buffer = '';
-	//receives an body
-	req.on('data', function(data){
-		// decode buffer object into a string
-		buffer += decoder.write(data);
-	});
-	req.on('end', function(){
+// Dependencies
+const server 			= require('./lib/server'),
+    	workers 		= require('./lib/workers');
 
-		buffer += decoder.end();
-		let data = {
-			'trimmedPath': trimmedPath, // the pathname
-			'queryStringObject': queryStringObject, // objects after the path name
-			'method': method, // [ get, post , put, delete ]
-			'headers': headers,
-			'payload': helpers.parseJsonToObject(buffer) // body
-		};
-
-		// base on the route that was chosen
-		chosenHandler(data, function(statusCode, payload){ // the callback comes from the router
-			// check if its a number
-			statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-			// check if its an object
-			payload = typeof(payload) == 'object'? payload : {};
-			// json to string
-			let payloadString = JSON.stringify(payload);
-			//parse this as if it was json
-			res.setHeader('Content-Type', 'application/json');
-			res.writeHead(statusCode);
-			res.end(payloadString);
-		});
-	});
+const app = {};
+// init function
+app.init =function(){
+	// start server
+	server.init();
+	// start workers
+	workers.init();
 };
 
 
+app.init();
 
-
-
-
-
-
-//Handlers
-		let router = {
-			'ping'  : handlers.ping,
-			'users' : handlers.users,
-			'tokens': handlers.tokens,
-			'checks': handlers.checks
-		};
+// export the app
+module.exports =app;
